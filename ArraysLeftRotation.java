d73e442b3129c67e62e4d77dfba7442c68816d68
Akhil Agrawal <agrawa49@purdue.edu>
2016-09-28 04:51:25 -0400

    Updated with solution

--- ArraysLeftRotation.java
+++ ArraysLeftRotation.java
@@ -9,10 +9,19 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int k = in.nextInt();
+        int d = in.nextInt();
         int a[] = new int[n];
         for(int a_i=0; a_i < n; a_i++){
             a[a_i] = in.nextInt();
         }
+        int temp;
+        for (int i = 0; i < d; i++) {
+            temp = a[0];
+            for (int j = 0; j < n - 1; j++)
+                a[j] = a[j + 1];
+            a[n - 1] = temp;
+        }
+        for (int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
     }
 }
